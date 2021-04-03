adc68b08e0ad29e58664f2e736bb3aa78749946b
rajasri1289 <47939295+rajasri1289@users.noreply.github.com>
2021-04-03 22:38:10 +0530

    Add files via upload

---
+++ TranslateText.py
@@ -0,0 +1,77 @@
+import os
+import argparse
+from googletrans import Translator
+
+def rmv_old_translated():
+    mypath =  os.path.dirname(os.path.abspath(__file__))
+    for f in os.listdir(mypath):
+        if f.startswith("translated"):
+            os.remove(f)
+            
+def rmv_old_temps():
+    mypath =  os.path.dirname(os.path.abspath(__file__))
+    for f in os.listdir(mypath):
+        if f.startswith("temp"):
+            os.remove(f)
+
+class Translation:
+
+    def __init__(self, *a):
+        self.from_language = args.from_language
+        self.to_language = args.to_language
+        self.extract_file = extract_file
+        self.translate_file = translate_file
+        self.transformed_file = transformed_file
+        with open(self.extract_file, 'a') as f:
+            f.write('\n')
+	    
+
+    def clean_text(self):
+        text = open(self.extract_file, 'r').read()
+        text = text.replace('\n', '\n ')
+        text = text.replace('\n', '')
+        text = text.replace('.', '.\n')
+        text = text.replace('!', '!\n')
+        text = text.replace('?', '?\n')
+        f = open(self.transformed_file, 'w')
+        f.write(text)
+        
+    def translate_text(self, text):
+
+        translator = Translator()
+        from_language = self.from_language
+        to_language = self.to_language
+        translation = translator.translate(text, 
+            src=from_language, dest=to_language).text.encode("utf-8")
+        return translation
+
+
+    def write_translation(self):
+        f1 = open(self.transformed_file, 'r')
+        with open(self.translate_file, 'w') as f:
+            for i, line in enumerate(f1, 1):
+                f.write(str(i) + ') ' + str(line.strip()) + '\n')
+                f.write(str(i) + ') ' + str(self.translate_text(line.strip()))+ '\n\n')
+        f.close()
+
+    
+if __name__ == "__main__":
+    rmv_old_translated()
+    mypath =  os.path.dirname(os.path.abspath(__file__))
+    for f in os.listdir(mypath):
+        if f.endswith(".txt"):
+            argparser = argparse.ArgumentParser()
+            argparser.add_argument('from_language',help = 'Example of command to run: python TranslateText.py fr en')
+            argparser.add_argument('to_language',help = 'Example of command to run: python TranslateText.py fr en')
+            args = argparser.parse_args()
+            extract_file = "{}.txt".format(os.path.splitext(f)[0])
+            transformed_file = "temp_{}.txt".format(os.path.splitext(f)[0])
+            translate_file = "translated_{}_{}_{}.txt".format(args.from_language,
+                args.to_language, os.path.splitext(f)[0])
+            t=Translation()
+            t.clean_text()
+            t.write_translation()
+            rmv_old_temps()
+            print('translation complete - check text file that starts with translated')
+
+
